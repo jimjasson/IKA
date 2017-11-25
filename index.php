1257908e21eba202428047fcdae17bf6df1f50e7
Jason Kytros <jimjasson@outlook.com.gr>
2017-11-25 18:30:25 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,35 @@
+<html>
+
+<head>
+
+<link rel="stylesheet" type="text/css" href="assets/css/style.css" media="screen" />
+
+
+<div class="logo">
+	<img src="data/images/ika.jpg">
+</div>
+<div class="top_corner_register">
+	<button class="register_button"><span> Εγγραφή </span></button>
+</div>
+<div class="top_corner_login">
+	<button class="login_button"><span> Σύνδεση </span></button>
+</div>
+</head>
+
+<body>
+
+<div id='top_nav_menu'>
+<ul>
+   <li class='active'><a href='#'><span>ΑΡΧΙΚΗ ΣΕΛΙΔΑ</span></a></li>
+   <li><a href='#'><span>ΑΣΦΑΛΙΣΜΕΝΟΙ</span></a></li>
+   <li class='last'><a href='#'><span>ΣΥΝΤΑΞΙΟΥΧΟΙ</span></a></li>
+   <li> 
+   	<form action="#" method="get"> </li>
+ 		<input type="text" name="search" placeholder="Αναζήτηση...">
+     	<button class="search_button" type="submit" style="cursor:pointer;"> Πάμε! </button> 
+   </form>
+</ul>
+</div>
+</body>
+
+</html>
