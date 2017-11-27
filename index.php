0128c0352c72cc3866129736e00ccd264c5ddce1
Jason Kytros <jimjasson@outlook.com.gr>
2017-11-27 20:42:32 +0200

    Added user-specific categories in the home page

--- index.php
+++ index.php
@@ -6,7 +6,9 @@
 
 
 <div class="logo">
-	<img src="data/images/ika.jpg">
+  <a href="/IKA/index.php">
+	   <img src="data/images/ika.jpg">
+  </a>
 </div>
 <div class="top_corner_register">
 	<button class="register_button"><span> Εγγραφή </span></button>
@@ -14,21 +16,38 @@
 <div class="top_corner_login">
 	<button class="login_button"><span> Σύνδεση </span></button>
 </div>
+
 </head>
 
 <body>
 
 <div id='top_nav_menu'>
-<ul>
-   <li class='active'><a href='#'><span>ΑΡΧΙΚΗ ΣΕΛΙΔΑ</span></a></li>
-   <li><a href='#'><span>ΑΣΦΑΛΙΣΜΕΝΟΙ</span></a></li>
-   <li class='last'><a href='#'><span>ΣΥΝΤΑΞΙΟΥΧΟΙ</span></a></li>
-   <li> 
-   	<form action="#" method="get"> </li>
- 		<input type="text" name="search" placeholder="Αναζήτηση...">
-     	<button class="search_button" type="submit" style="cursor:pointer;"> Πάμε! </button> 
-   </form>
-</ul>
+  <ul>
+     <li class='active'><a href='#'><span>ΑΡΧΙΚΗ ΣΕΛΙΔΑ</span></a></li>
+     <li><a href='#'><span>ΑΣΦΑΛΙΣΜΕΝΟΙ</span></a></li>
+     <li class='last'><a href='#'><span>ΣΥΝΤΑΞΙΟΥΧΟΙ</span></a></li>
+     <li> 
+     	<form action="#" method="get"> </li>
+   		<input type="text" name="search" placeholder="Αναζήτηση...">
+       	<button class="search_button" type="submit" style="cursor:pointer;"> Πάμε! </button> 
+     </form>
+  </ul>
+</div>
+
+<div class="home_menu_wrapper">
+  <div class="home_left_menu">
+    <a href="/IKA/index.php">
+      <img src="data/images/asfalismenoi_banner.jpg">
+      <div class="centered">Ασφαλισμένοι</div>
+    </a>
+  </div>
+  
+  <div class="home_right_menu">
+    <a href="/IKA/index.php">
+      <img src="data/images/syntaxiouxoi_banner.jpg">
+      <div class="centered">Συνταξιούχοι</div>
+    </a>
+  </div>
 </div>
 </body>
 
