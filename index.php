9c6554438674abd617ac7a79094bd35cf1040d5b
Jason Kytros <jimjasson@outlook.com.gr>
2017-11-30 18:58:45 +0200

    Added sidebar and footer

--- index.php
+++ index.php
@@ -55,6 +55,34 @@
       <div class="centered">Συνταξιούχοι</div>
     </a>
   </div>
+
+  <div class="sidebar">
+  <h1 class="announcments_title"> Ανακοινώσεις </h1>
+  <div class="announcments_seperator"></div>
+  <div class="announcments_context">
+    <p class="announcments_title" align="justify"> Καλως ορίσατε στην καινούρια μας ιστοσελίδα! </p>
+    <p class="announcments_date" align="justify"> 15 Ιανουαρίου 2018 </p>
+    <p class="announcments_body" align="justify"> Σας παρουσιάζουμε τη νέα ιστοσελίδα του Ι.Κ.Α. </p>
+  </div>
+  </div>
+
+  <div class="footer">
+    <div class="contact">
+      <p class="title"> Επικοινωνία </p>
+    </div>
+    <div class="schedule">
+      <p class="title"> Ωράριο Καταστημάτων </p>
+    </div>
+    <div class="sitemap">
+      <p class="title"> Sitemap </p>
+    </div>
+    <div class="map_context">
+      <p class="title"> Αναζητήστε το κοντινότερο γραφείο ΙΚΑ! </p>
+    </div>
+    <div class="map">
+     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d50314.89399439727!2d23.69164977910156!3d37.9845762!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14a1bd2ff04f0f75%3A0xfb57f9bf53a6a952!2zzpnOms6RIC0gzpXOpM6Rzpw!5e0!3m2!1sel!2sgr!4v1512060477783" width="200" height="150" frameborder="0" style="border:0" allowfullscreen></iframe>
+   </div>
+  </div>
 </div>
 </body>
 
