32e19b02e0726ba4ac247b05239798e752ab3f12
Jason Kytros <jimjasson@outlook.com.gr>
2017-12-04 19:55:59 +0200

    Added footer

--- index.php
+++ index.php
@@ -4,12 +4,15 @@
 
 <link rel="stylesheet" type="text/css" href="assets/css/style.css" media="screen" />
 
-
+<!-- Logo --> 
 <div class="logo">
   <a href="/IKA/index.php">
 	   <img src="data/images/ika.jpg">
   </a>
 </div>
+
+<!-- Register and Login --> 
+
 <div class="top_corner_register">
 	<button class="register_button"><span> Εγγραφή </span></button>
 </div>
@@ -20,6 +23,8 @@
 </head>
 
 <body>
+
+<!-- Top navigation menu --> 
 
 <div id='top_nav_menu'>
   <ul>
@@ -56,6 +61,8 @@
     </a>
   </div>
 
+<!--Sidebar --> 
+
   <div class="sidebar">
   <h1 class="announcments_title"> Ανακοινώσεις </h1>
   <div class="announcments_seperator"></div>
@@ -66,12 +73,18 @@
   </div>
   </div>
 
+<!-- Footer --> 
+
   <div class="footer">
     <div class="contact">
       <p class="title"> Επικοινωνία </p>
+      <img src="data/images/phone.png">
+      <p class="number"> 2101234567 </p>
     </div>
     <div class="schedule">
       <p class="title"> Ωράριο Καταστημάτων </p>
+      <img src="data/images/office.png">
+      <p class="time"> Δευτέρα - Παρασκευή 09:00 - 14:00</p>
     </div>
     <div class="sitemap">
       <p class="title"> Sitemap </p>
@@ -83,7 +96,9 @@
      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d50314.89399439727!2d23.69164977910156!3d37.9845762!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14a1bd2ff04f0f75%3A0xfb57f9bf53a6a952!2zzpnOms6RIC0gzpXOpM6Rzpw!5e0!3m2!1sel!2sgr!4v1512060477783" width="200" height="150" frameborder="0" style="border:0" allowfullscreen></iframe>
    </div>
   </div>
+
 </div>
+
 </body>
 
 </html>
