669b1fddf4de250643c50233e12d6c6da3ec4ef5
Jason Kytros <jimjasson@outlook.com.gr>
2018-01-11 19:51:30 +0200

    Minor CSS fix in the insured.php file

--- pages/insured.php
+++ pages/insured.php
@@ -61,8 +61,8 @@ $_SESSION['url'] = $_SERVER['REQUEST_URI'];
 <div id='top_nav_menu'>
 	<ul>
 		 <li><a href='/IKA/index.php'><span>ΑΡΧΙΚΗ ΣΕΛΙΔΑ</span></a></li>
-		 <li class='active last'><a href='#'><span>ΑΣΦΑΛΙΣΜΕΝΟΙ</span></a></li>
-		 <li><a href='/IKA/pages/pensioners.php'><span>ΣΥΝΤΑΞΙΟΥΧΟΙ</span></a></li>
+		 <li class='active'><a href='#'><span>ΑΣΦΑΛΙΣΜΕΝΟΙ</span></a></li>
+		 <li class='last'><a href='/IKA/pages/pensioners.php'><span>ΣΥΝΤΑΞΙΟΥΧΟΙ</span></a></li>
 		 <li> 
 			<form action="#" method="get"> </li>
 			<input type="text" name="search"  class="search_field" placeholder="Αναζήτηση...">
