2df0e8a22c2684c091d731c566e637be81131143
Jason Kytros <jimjasson@outlook.com.gr>
2018-01-13 20:26:01 +0200

    Update database

--- sdi1400220.sql
+++ sdi1400220.sql
@@ -2,10 +2,10 @@
 -- version 4.7.4
 -- https://www.phpmyadmin.net/
 --
--- Φιλοξενητής: 127.0.0.1
--- Χρόνος δημιουργίας: 13 Ιαν 2018 στις 17:00:24
--- Έκδοση διακομιστή: 10.1.29-MariaDB
--- Έκδοση PHP: 7.2.0
+-- Φιλοξενητής: localhost
+-- Χρόνος δημιουργίας: 13 Ιαν 2018 στις 19:25:36
+-- Έκδοση διακομιστή: 10.1.28-MariaDB
+-- Έκδοση PHP: 7.1.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -58,20 +58,27 @@ CREATE TABLE `insurance_info` (
   `AFM` char(9) CHARACTER SET latin1 COLLATE latin1_general_ci NOT NULL,
   `BIRTH_DATE` date NOT NULL,
   `INSUR_DATE` date NOT NULL,
+  `PENS_DATE` date NOT NULL,
   `WORKHOURS` int(11) NOT NULL,
   `INSUR_TYPE` int(11) NOT NULL,
   `CHILDREN` int(11) NOT NULL,
-  `PENSION_AMOUNT` int(11) NOT NULL
+  `PENSION_AMOUNT` int(11) NOT NULL,
+  `INSURED_CHILDREN` int(11) NOT NULL,
+  `YEAR1` int(11) NOT NULL,
+  `YEAR2` int(11) NOT NULL,
+  `YEAR3` int(11) NOT NULL,
+  `YEAR4` int(11) NOT NULL,
+  `YEAR5` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Άδειασμα δεδομένων του πίνακα `insurance_info`
 --
 
-INSERT INTO `insurance_info` (`AFM`, `BIRTH_DATE`, `INSUR_DATE`, `WORKHOURS`, `INSUR_TYPE`, `CHILDREN`, `PENSION_AMOUNT`) VALUES
-('000000000', '1992-05-12', '2011-11-01', 500, 0, 0, 0),
-('123456789', '1949-03-16', '1971-02-22', 5500, 0, 0, 0),
-('987654321', '1956-10-10', '1980-10-10', 3000, 0, 0, 0);
+INSERT INTO `insurance_info` (`AFM`, `BIRTH_DATE`, `INSUR_DATE`, `PENS_DATE`, `WORKHOURS`, `INSUR_TYPE`, `CHILDREN`, `PENSION_AMOUNT`, `INSURED_CHILDREN`, `YEAR1`, `YEAR2`, `YEAR3`, `YEAR4`, `YEAR5`) VALUES
+('000000000', '1992-05-12', '2011-11-01', '0000-00-00', 500, 0, 0, 0, 0, 0, 1000, 3500, 6500, 6500),
+('123456789', '1949-03-16', '1971-02-22', '0000-00-00', 7500, 0, 3, 0, 0, 13000, 14000, 13500, 15000, 14000),
+('987654321', '1956-10-10', '1980-10-10', '0000-00-00', 6000, 0, 2, 0, 0, 11000, 11000, 16000, 16000, 16500);
 
 --
 -- Ευρετήρια για άχρηστους πίνακες
